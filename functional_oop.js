40dace14d4836cfd05e663542b1eaf9de70888f1
Ndeta Innocent <you@example.com>
2022-01-23 16:36:30 +0200

    Idempotence

--- functional_oop.js
+++ functional_oop.js
@@ -20,6 +20,15 @@ const array=[1,2,3]
 function mutateArray(arr){
    arr.pop()
 }
+function removeLastItem(arr){
+    const newArray=[].concat(arr)
+    newArray.pop()
+    return newArray 
+}
+function multiplyBY2(arr){
+    return arr.map(item=>item*2)
+}
+multiplyBY2(array)
 mutateArray(array)
 mutateArray(array)
 function mutateArrayNew(arr){
@@ -27,3 +36,19 @@ function mutateArrayNew(arr){
  }
  mutateArrayNew(array)
  mutateArrayNew(array)
+
+//  referential transparency
+function b(num){
+    return num *2
+}
+b(5)
+function a(num, num1){
+    return num*num1
+}
+a(3,5)
+
+// Idempotence ie giving the same input
+function noGood(num){
+    return Math.random(num)
+}
+noGood(4)
